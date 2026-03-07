b38b8c7d0b40c72c08c8d113c01cce60f444e91a
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-07 23:14:32 +0100

    chore(release): bump version to 1.0.0-alpha3
    Release version 1.0.0-alpha3
    
    fix: Remove reference from signatures.
    Mark as a H6 app.
    Add CI job for satis repo update
    Development mode for operator-1.0.0alpha3

--- lib/Application.php
+++ lib/Application.php
@@ -37,7 +37,7 @@ class Operator_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H6 (1.0.0alpha2)';
+    public $version = '1.0.0-alpha3';
 
     /**
      * TODO
